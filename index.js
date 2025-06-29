aa339bf7005d77e371c2049a5735834412f32c39
MichelleKamau6 <michwk06@gmail.com>
2025-06-29 15:37:05 +0300

    Document

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return 'This one is on me!';
-  } else if (feet > 2000 && feet <= 2500) {
-    return 'I will gladly take your thirty bucks.';
   } else if (feet > 2500) {
     return 'No can do.';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
   }
 }
 
-
-function ternaryCheckCity(){
-   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
       return 'Thank you so much.';
@@ -23,4 +22,7 @@ function switchOnCharmFromTip(){
       return 'Bye.';
   }
 }
+
+
+
 
