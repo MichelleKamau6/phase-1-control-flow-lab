2266ecf8026378c7b16b7ac2418337e2a4e5d170
MichelleKamau6 <michwk06@gmail.com>
2025-06-29 15:43:27 +0300

    Same1

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-if (feet > 2000 && feet <= 2500) { ... }
-
 
 function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return 'This one is on me!';
-  } else if (feet > 2500) {
-    return 'No can do.';
+  } else if (feet > 400 && feet <= 2000) {
+    return 'That will be twenty bucks.';
   } else if (feet > 2000 && feet <= 2500) {
     return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
   }
 }
 
 function ternaryCheckCity(city) {
-  return city === "NYC" ? "Ok, sounds good." : "No go.";
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
+
 
 function switchOnCharmFromTip(tip) {
   switch (tip) {
